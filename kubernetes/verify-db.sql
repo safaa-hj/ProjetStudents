deb8407a14481fe68985a8c9d3347352fcb49ed0
safa hajji <122635442+safaa-hj@users.noreply.github.com>
2025-12-10 10:33:36 +0100

    Add Kubernetes deployment files and configurations

---
+++ kubernetes/verify-db.sql
@@ -0,0 +1,19 @@
+USE mydb;
+
+-- Show all tables
+SHOW TABLES;
+
+-- Show department table structure
+DESCRIBE department;
+
+-- Show all departments
+SELECT * FROM department;
+
+-- Show all students
+SELECT * FROM student;
+
+-- Show enrollment information
+SELECT * FROM enrollment;
+
+-- Show courses
+SELECT * FROM course;
